864b4e8968c6c70246695df158e46c82dd7e5887
lupuuss <damian.baczynski@gmail.com>
2020-04-15 15:50:19 +0200

    Removes unused xmodem exceptions

--- src/mingwMain/kotlin/xmodem/protocol/Exceptions.kt
+++ src/mingwMain/kotlin/xmodem/protocol/Exceptions.kt
@@ -2,10 +2,6 @@ package xmodem.protocol
 
 abstract class XmodemException(msg: String?, cause: Throwable? = null) : Exception(msg, cause)
 
-class XmodemSenderTimeout(time: Long) : XmodemException("Sender didn't answer in $time ms!")
-
 class XmodemIOException(cause: Throwable) : XmodemException(cause.message, cause)
 
 class XmodemCancelException(state: State.Cancel) : XmodemException("Transmission canceled! State: $state", null)
-
-class XmodemRetriesLimitReached(limit: Int) : XmodemException("Retries limit reached! Limit: $limit")
