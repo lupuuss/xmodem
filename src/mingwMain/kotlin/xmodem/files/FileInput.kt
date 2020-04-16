7abed21e2b230bdd9cc4b54fd23f72eb634761e5
lupuuss <damian.baczynski@gmail.com>
2020-04-17 01:00:31 +0200

    Adds FileInput (wrapping C fread and FILE)

---
+++ src/mingwMain/kotlin/xmodem/files/FileInput.kt
@@ -0,0 +1,38 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE", "EXPERIMENTAL_UNSIGNED_LITERALS")
+
+package xmodem.files
+
+import kotlinx.cinterop.*
+import platform.posix.*
+
+class FileInput(
+    private val path: String,
+    private val mode: Mode
+) {
+    enum class Mode(val str: String) {
+        Binary("rb"), Text("r")
+    }
+
+    private lateinit var file: CPointer<FILE>
+
+    fun open() {
+        val tmp = fopen(path, mode.str)
+
+        if (tmp == null) {
+            throw FileOpenException(path)
+        } else {
+            file = tmp
+        }
+    }
+
+    fun read(n: Int) = memScoped {
+        val buffer = allocArray<ByteVar>(n)
+
+        fread(buffer.pointed.ptr, 1u, n.toULong(), file )
+    }
+
+    fun close() {
+        fclose(file)
+    }
+
+}
